334f1d5eb261bfd04bbf90dc1768918b0592bce4
Aaron Kidwell <aaronkidwell95@outlook.com>
2026-05-18 17:36:45 -0400

    deleted trash

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module dev
 
 go 1.26.2
 
-require golang.org/x/sys v0.20.0
+require golang.org/x/sys v0.44.0
--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println("Operating System:", runtime.GOOS)
 	fmt.Println("Architecture:", runtime.GOARCH)
 	fmt.Println("CPU Cores:", runtime.NumCPU())
-	fmt.Println("Hostname:", windows.GetComputerName())
+	hostname, err := windows.ComputerName()
+	if err != nil {
+		fmt.Println("Hostname:", err)
+	} else {
+		fmt.Println("Hostname:", hostname)
+	}
 
 }
